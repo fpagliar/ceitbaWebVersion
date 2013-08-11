440701d01f205874f63c8274f3dafa4e970e6c40
fpagliar <fpagliar@redmintlabs.com>
2013-08-11 20:17:10 -0300

    is this form needed?

---
+++ src/main/java/ar/edu/itba/paw/presentation/command/SearchServiceForm.java
@@ -0,0 +1,18 @@
+package ar.edu.itba.paw.presentation.command;
+
+public class SearchServiceForm {
+	
+	String service;
+	
+	public SearchServiceForm(){
+		
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+}
